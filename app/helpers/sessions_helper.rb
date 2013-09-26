21460ea046a633ecf7cc2abe35549b53488a31fb
Sachin Virdy <svirdy1@gmail.com>
2013-09-26 19:20:16 -0400

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,22 +11,41 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_back_or user
+		else
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+	
 	def current_user=(user)
 		@current_user = user
 	end
 
 	def current_user
-		remember_token = User.encrypt(cookies[remember_token])
+		remember_token = User.encrypt(cookies[:remember_token])
 		@current_user ||= User.find_by(remember_token: remember_token)
+	end
+
+	def current_user?(user)
+		user == current_user
 	end
 
 	def sign_out
 		self.current_user = nil
 		cookies.delete(:remember_token)
 	end
-end
 
-# First, create a new token 
-# Second, place the unencrypted token in the browser cookies
-# Third, save the encrypted token to the database
-# Fourth, set the current user equal to the given user
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url if request.get?
+	end
+end
